c6b277da21f07a337b9848018ea502bb50895373
Sungwoo Park <glapark@datamonad.com>
2025-06-11 07:57:25 -0700

    Disable keep-alive for HTTP connections used by Fetchers.   tez.shuffle.connection-keep-alive.enable is set to false (on the shuffle server side).   tez.runtime.shuffle.keep-alive.enabled is set to false (on the shuffle client side).
    Experiments show that on the 10TB TPC-DS bnechmark, disabling keep-alive improves
    the performance and stability.

--- typescript/src/server/validate/config.ts
+++ typescript/src/server/validate/config.ts
@@ -66,7 +66,7 @@ export function initial(): T {
       'tez.shuffle-vertex-manager.auto-parallel.max.reduction.percentage': 50,
       'tez.shuffle-vertex-manager.use-stats-auto-parallelism': true,
       'tez.shuffle.vertex.manager.auto.parallelism.min.percent': 10,
-      'tez.shuffle.connection-keep-alive.enable': true,
+      'tez.shuffle.connection-keep-alive.enable': false,
       'tez.shuffle.listen.queue.size': 16384,
       'tez.shuffle.mapoutput-info.meta.cache.size': 10000,
       'tez.runtime.shuffle.connect.timeout': 7500,
